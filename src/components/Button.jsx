71b2955f308c1facf6e37c813b5f233cee2d41f9
Timothy <oludefiyinfoluwa06@gmail.com>
2024-07-29 10:25:50 +0100

    add apk file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,22 @@
 // eslint-disable-next-line react/prop-types
 const Button = ({ text }) => {
+    const handleDownload = () => {
+        const fileUrl = '../assets/xrh.apk';
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.download = 'Xceloft-Realty-Hub.apk';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
-        <button className="bg-gradient-to-r from-blue to-[#3E37A3] text-white px-4 py-2 rounded">{text}</button>
+        <button 
+            className="bg-gradient-to-r from-blue to-[#3E37A3] text-white px-4 py-2 rounded"
+            onClick={handleDownload}
+        >
+            {text}
+        </button>
     );
 }
 
